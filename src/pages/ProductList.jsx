277f52fa0b640a6c389f676dcf0567cd85af6919
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2023-12-24 18:43:37 +0300

    React project oluşturuldu, Api ile bağlantısı yapıldı

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import ProductService from "../services/productService";
+
+export default function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    let productService = new ProductService();
+    productService
+      .getProducts()
+      .then((result) => setProducts(result.data.data));
+  });
+
+  return (
+    <div>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>productId</Table.HeaderCell>
+            <Table.HeaderCell>categoryId</Table.HeaderCell>
+            <Table.HeaderCell>Ürün Adı</Table.HeaderCell>
+            <Table.HeaderCell>Ürün Stok Bilgisi</Table.HeaderCell>
+            <Table.HeaderCell>Ürün Fiyatı</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {products.map((product) => (
+            <Table.Row key={product.productId}>
+              <Table.Cell>{product.productId}</Table.Cell>
+              <Table.Cell>{product.categoryId}</Table.Cell>
+              <Table.Cell>{product.productName}</Table.Cell>
+              <Table.Cell>{product.unitsInStock}</Table.Cell>
+              <Table.Cell>{product.unitPrice}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="3">
+              <Menu floated="right" pagination>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron left" />
+                </Menu.Item>
+                <Menu.Item as="a">1</Menu.Item>
+                <Menu.Item as="a">2</Menu.Item>
+                <Menu.Item as="a">3</Menu.Item>
+                <Menu.Item as="a">4</Menu.Item>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron right" />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </div>
+  );
+}
